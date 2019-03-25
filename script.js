e4543d343de58cd6860809a33b8bf4e1b713a173
msuero1906 <43377325+msuero1906@users.noreply.github.com>
2019-03-25 08:43:40 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$("button").click(function(){
+    var picture=$(".picture").val();
+
+$(".answer").text();
+   
+    if(picture<=3 ) {
+	$("p").text("You Are Timothy Mouse!!!");}
+	
+	else if(picture >=4 && picture<=5) {
+	$("p").text("You Are Mr.Stork!!!");}
+
+	else if(picture >=6) {
+	 $("p").text("You Are Dumbo!!!");}
+
+  });
